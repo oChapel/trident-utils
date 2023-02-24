2de1a9c10bcd588dcc91936de953cdf1510bee7a
o_chapel <8799@ukr.net>
2023-02-24 17:48:32 +0200

    Add new TextUtils functions

--- utils/src/main/java/com/och/trident_utils/TextUtils.kt
+++ utils/src/main/java/com/och/trident_utils/TextUtils.kt
@@ -1,5 +1,7 @@
 package com.och.trident_utils
 
 object TextUtils {
+    fun emptyString(): String = ""
     fun String.isNotEmptyOrNull(): Boolean = this.isNotEmpty() && this != "null"
+    fun String.newUserAgentString(): String = this.replace("wv", "")
 }
